87378e8da18ddcc7632db349d07d0d72a041ee0e
Elco Jacobs <elcojacobs@gmail.com>
2015-03-24 18:52:20 +0100

    auto detect serial port after programming, print version received and send EEPROM reset command to device

--- utils/flash-dfu.py
+++ utils/flash-dfu.py
@@ -22,14 +22,19 @@ if sys.version_info < (2, 7):
 
 # standard libraries
 import time
-import socket
-import os
 import getopt
-from pprint import pprint
-import shutil
-import traceback
-import urllib
 import subprocess
+import serial
+import os
+
+# choose an implementation, depending on os (copied from serial.tools.list_ports)
+if os.name == 'nt': #sys.platform == 'win32':
+    import serial.tools.list_ports_windows as serial_tools
+elif os.name == 'posix':
+    import serial.tools.list_ports_posix as serial_tools
+else:
+    raise ImportError("Sorry: no implementation for your platform ('%s') available" % (os.name,))
+
 
 # Read in command line arguments
 try:
@@ -72,6 +77,19 @@ if not output:
     print "dfu-util cannot be found, please add its location to your PATH variable"
     exit(1)
 
+def all_ports():
+    result = []
+    for port in serial_tools.comports():
+        result.append(port[0])
+    return result
+
+def new_ports(old_ports):
+    result = []
+    for port in serial_tools.comports():
+        if not port[0] in old_ports:
+            result.append(port[0])
+    return result
+
 while(True):
     # list DFU devices to see whether a device is connected
     p = subprocess.Popen("dfu-util -l", stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True)
@@ -83,12 +101,54 @@ while(True):
         # found a DFU device, flash the binary file to it
         if binFile:
             print "found DFU device, now flashing %s \n\n" % binFile
-
+            existing_ports = all_ports() # get existing ports while in DFU mode, because it will exclude the port we are looking for
             p = subprocess.Popen("dfu-util -d 1d50:607f -a 0 -s 0x08005000:leave -D %s" % binFile, shell=True)
             p.wait()
+            print "Waiting for new serial ports"
+            ser = None
+            for i in range(0,10):
+                newPorts = new_ports(existing_ports)
+                if newPorts:
+                    port = newPorts[0] # assume device is first newly found port
+                    try:
+                        print("Opening serial port {0}".format(port))
+                        ser = serial.Serial(port, 57600, timeout=1)
+                    except (OSError, serial.SerialException) as e:
+                            print("Error opening serial port {0}. {1}".format(port, str(e)))
+
+                    if not ser:
+                        continue
+
+                    # wait until valid version string received. Device can be waiting in touch screen calibration
+                    print "Waiting until I receive the version info from the device"
+                    while 1:
+                        ser.write('n')
+                        answer = ser.readline()
+                        if answer and answer[0] == 'N':
+                            print "Version: {0}".format(answer)
+                            break
+
+                    while ser.readline():
+                        pass # discard leftover lines
+
+                    print "Resetting EEPROM"
+                    ser.write('E\n')
+                    answer = ser.readline()
+                    if "\"logID\":15" in answer:
+                        print "Successfully initialized EEPROM"
+                    else:
+                        print "Received unexpected reply: {0}".format(answer)
+                    break
+                else:
+                    time.sleep(1)
+            else:
+                # i reached 10, timeout
+                print "could not open serial port of newly programmed device, EEPROM not reset"
         else:
             print "found DFU device, but no binary specified for flashing"
         if not multi:
             break
+        else:
+            print "Waiting for next device...\n"
 
     time.sleep(1)
